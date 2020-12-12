98ada810a98e90ff2dd6828d25ae8781c9ea0a42
RucioTozzy <fallgray@gmail.com>
2020-12-12 13:16:02 +0800

    Nuke DeviceSettings
    [ref]: https://github.com/jhonboy121/android_device_oneplus_sm8150-common/commit/a93044377a2e232413010eae8b5ab7783cef68e3

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -16,29 +16,29 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
+#LOCAL_PATH := $(call my-dir)
+#include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVATE_PLATFORM_APIS := true
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_PACKAGE_NAME := DeviceSettings
+#LOCAL_SRC_FILES := $(call all-java-files-under, src)
+#LOCAL_CERTIFICATE := platform
+#LOCAL_PRIVATE_PLATFORM_APIS := true
+#LOCAL_PRIVILEGED_MODULE := true
+#LOCAL_PACKAGE_NAME := DeviceSettings
 
-LOCAL_USE_AAPT2 := true
+#LOCAL_USE_AAPT2 := true
 
-LOCAL_STATIC_ANDROID_LIBRARIES := \
-    androidx.core_core \
-    androidx.preference_preference 
+#LOCAL_STATIC_ANDROID_LIBRARIES := \
+#    androidx.core_core \
+#    androidx.preference_preference 
 
-LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res \
-    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res \
-    $(LOCAL_PATH)/../../../../packages/resources/devicesettings-custom/res
+#LOCAL_RESOURCE_DIR := \
+#    $(LOCAL_PATH)/res \
+#    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res \
+#    $(LOCAL_PATH)/../../../../packages/resources/devicesettings-custom/res
 
-LOCAL_AAPT_FLAGS := --auto-add-overlay
+#LOCAL_AAPT_FLAGS := --auto-add-overlay
 
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-LOCAL_MODULE_TAGS := optional
-include frameworks/base/packages/SettingsLib/common.mk
-include $(BUILD_PACKAGE)
+#LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+#LOCAL_MODULE_TAGS := optional
+#include frameworks/base/packages/SettingsLib/common.mk
+#include $(BUILD_PACKAGE)
